5ef4d7921f8dbd5c71fe9f8515d359b4708edf3f
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2023-11-23 04:26:42 +0600

    get current user endpoint

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -8,8 +8,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.config import settings
 from app.core.db import get_db
 from app.core.exceptions import UnauthorizedException
-from app.core.utils import authenticate, create_access_token
-from app.schemas.user import TokenResponse
+from app.core.utils import authenticate, create_access_token, get_current_user
+from app.models.user import User
+from app.schemas.user import TokenResponse, UserResponse, UserSchema
 
 router = APIRouter()
 
@@ -27,3 +28,8 @@ async def login_for_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "Bearer"}
+
+
+@router.get('/users/me', response_model=UserResponse)
+async def get_current_user(current_user: Annotated[UserSchema, Depends(get_current_user)]):
+    return current_user
