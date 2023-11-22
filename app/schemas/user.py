ed5db7eba076aed139004949d309c33cdda33b01
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2023-11-23 03:37:24 +0600

    user schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+
+
+class UserSchema(BaseModel):
+    email: str = Field(title="", description="")
+    password: str = Field(title="", description="")
+
+
+class LoginResponse(BaseModel):
+    access_token: str = Field(title="", description="")
+
+
+class UserResponse(BaseModel):
+    id: int = Field(title="", description="")
+    email: int = Field(title="", description="")
