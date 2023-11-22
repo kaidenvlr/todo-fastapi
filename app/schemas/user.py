e08fd6dfd379be0a1271ef057f3a9c8739cb2ec6
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2023-11-23 04:00:50 +0600

    Token Data Schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -10,6 +10,10 @@ class LoginResponse(BaseModel):
     access_token: str = Field(title="", description="")
 
 
+class TokenDataSchema(BaseModel):
+    username: str | None = Field(title="", description="")
+
+
 class UserResponse(BaseModel):
     id: int = Field(title="", description="")
     email: int = Field(title="", description="")
