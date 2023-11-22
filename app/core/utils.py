c3bc911f446e1d154aed46beed9b3378a0d81f03
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2023-11-23 04:00:21 +0600

    JWT Authenticate functions

---
+++ app/core/utils.py
@@ -0,0 +1,55 @@
+from datetime import timedelta, datetime
+from typing import Annotated
+
+from fastapi import Depends
+from jose import jwt, JWTError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.config import pwd_context, settings, oauth2_scheme
+from app.core.db import get_db
+from app.core.exceptions import UnauthorizedException, BadRequestException
+from app.models.user import User
+from app.schemas.user import TokenDataSchema
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+async def authenticate(db: AsyncSession, username, password):
+    user = await User.get_by_email(username, db)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expires = datetime.utcnow() + expires_delta
+    else:
+        expires = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expires})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(db: Annotated[AsyncSession, Depends(get_db)], token: Annotated[User, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise UnauthorizedException()
+        token_data = TokenDataSchema(username=username)
+    except JWTError:
+        raise UnauthorizedException()
+    user = await User.get_by_email(token_data.username, db)
+    if user is None:
+        raise UnauthorizedException()
+    return user
